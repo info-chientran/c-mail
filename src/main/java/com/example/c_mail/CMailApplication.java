6c4f5568745f85313b8ee47b5dea5a644642d4b9
Chien Tran <info.chientran@gmail.com>
2025-02-19 15:02:26 +0700

    [config] Init project

---
+++ src/main/java/com/example/c_mail/CMailApplication.java
@@ -0,0 +1,13 @@
+package com.example.c_mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CMailApplication.class, args);
+	}
+
+}
